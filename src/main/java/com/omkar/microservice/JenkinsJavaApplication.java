14a5ea9611d6bcdc3846cfeb81bc169376bde15d
piddeomkar2017 <temp@gmail.com>
2021-12-29 07:52:38 -0800

    1st code

---
+++ src/main/java/com/omkar/microservice/JenkinsJavaApplication.java
@@ -0,0 +1,13 @@
+package com.omkar.microservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinsJavaApplication.class, args);
+	}
+
+}
